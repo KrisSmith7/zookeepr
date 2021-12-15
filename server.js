6423bf09072cde23eb026fade23441e51b721131
KrisSmith7 <krissmithcodes@gmail.com>
2021-12-15 17:21:18 -0500

    added front end and updated functionality with back end processes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path')
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -108,6 +109,19 @@ app.get('/api/animals', (req, res) => {
 
   res.json(animal);
       });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+    });
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+    });
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+    });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+    });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
